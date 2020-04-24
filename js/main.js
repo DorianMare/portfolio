8a6489da864b9e757a0f727d50a4bcf6858b3833
Mare Dorian <dorian.mare@etu.unilim.fr>
2020-04-24 13:35:01 +0200

    ajout nav, cormorant font et Qui suis-je ?

--- js/main.js
+++ js/main.js
@@ -1,2 +1,28 @@
 //animation from start
-gsap.from("#aboutMe", {duration: 2, opacity: 0, y: -120} );
+var tl_Start = gsap.timeline();
+tl_Start.from("#presentation", {duration: 1.5, opacity: 0, y: -100}).delay(0.5)
+    .from("nav", {duration: 1, opacity: 0, x: -500});
+
+//init the ScrollMagic controller
+var controller = new ScrollMagic.Controller();
+
+//create a tl and a scene
+var tl_WhoAmI = gsap.timeline();
+var scene = new ScrollMagic.Scene({
+    triggerElement: "#about"
+}).setTween(("#about", {duration: 1.5, opacity: 0, y: -100})).addTo(controller);
+
+
+//scroll detection (currently usueless)
+/*var lastScrollTop = window.pageYOffset || document.documentElement.scrollTop;
+window.addEventListener("scroll", function(){
+   var st = window.pageYOffset || document.documentElement.scrollTop;
+   if (st > lastScrollTop){
+      this.console.log("downscroll !");
+   }
+   else {
+       this.console.log("upscroll !");
+   }
+   lastScrollTop = st <= 0 ? 0 : st;
+}, false);
+*/
